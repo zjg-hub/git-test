50bf28edbe6307a997167669f56a8cd84a472ea6
ZJG <ZJG@example.com>
2022-04-10 17:34:34 +0800

    test 4

--- src/main/java/com/test/test.java
+++ src/main/java/com/test/test.java
@@ -7,5 +7,6 @@ public class test {
         System.out.println("hello git3");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hello git5");
     }
 }
